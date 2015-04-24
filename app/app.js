c05694d78a2e9d6c3cb8a2248500b456db28c0dc
dan <dan@dan-VGN-FW41J-H>
2015-04-24 08:35:20 +0200

    first commit

--- app/app.js
+++ app/app.js
@@ -7,21 +7,6 @@
 
 (function () {
 
-// application twittsbylocation
-// https://www.delfrantoio.com/portofino/twitts.html
-// Consumer Key (API Key) gkyPXmrbP8dYaChX2lf6jJLjY 
-// Consumer Secret (API Secret) iUYC9OVJnvb7UWeAcwXH364sAPDLa907ApVJA6kn32Bm5faVUe
-// Access Level Read and write (modify app permissions)
-// Owner dancentamore
-// Owner ID 2987914385
-
-
-//my twitter account
-//Access Token 2987914385-lpbrDBcXi8r505Zq6zU9JnF5Kqu7YWGdj3PgB8P
-//Access Token Secret Gr9BdmlBEXu999h6FfQIjQSnPaGFieunGTggDzeBmvm8V
-//Access Level Read and write
-//Owner dancentamore
-//Owner ID 2987914385
 
 
     // main module
